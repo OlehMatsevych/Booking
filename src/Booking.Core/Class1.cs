320f13ce48000a77b2f80042398cedfcb7a791c9
Oleh Matsevych <oleh_matsevych@epam.com>
2021-10-13 12:21:52 +0300

    Architecture

---
+++ src/Booking.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Booking.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Booking.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Booking.DataAccess
+{
+    public class Class1
+    {
+    }
+}
